5cf2718b11358dcc3d71301de40aa9ed854ce8df
parekhkunal <parekhkunal09@gmail.com>
2022-08-19 18:03:33 +0530

    Updates

---
+++ precache-manifest.c78054efce7c999bba68729354635e74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21dff84f3aa079bd8763bab1e4f0fd36",
+    "url": "/fsd-prac-3/index.html"
+  },
+  {
+    "revision": "4b8fc15d9ad7b815da3d",
+    "url": "/fsd-prac-3/static/css/main.1109858f.chunk.css"
+  },
+  {
+    "revision": "ddc342d28e30836a149b",
+    "url": "/fsd-prac-3/static/js/2.c7d02cc6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/fsd-prac-3/static/js/2.c7d02cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b8fc15d9ad7b815da3d",
+    "url": "/fsd-prac-3/static/js/main.c04380b0.chunk.js"
+  },
+  {
+    "revision": "40e63f22c85002c5258d",
+    "url": "/fsd-prac-3/static/js/runtime-main.87d6f3fd.js"
+  }
+]);
