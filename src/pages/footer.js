75f35652db2219706f1b13e8d6eb3c83e3d7e172
parekhkunal <parekhkunal09@gmail.com>
2022-08-19 18:03:58 +0530

    First Commit

---
+++ src/pages/footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div>
+          <h2 style={{
+              position: 'fixed',
+              bottom: '0',
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              backgroundColor: 'black',
+              color: 'white',
+              padding: '10px'
+          }}>20dcs061 - Kunal Parekh</h2>
+    </div>
+  )
+}
+
+export default footer
